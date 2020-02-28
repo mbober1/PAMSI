149eb8ea16783e719a5d6326de27dac176caa294
Marcin Bober <mbober@pop-os.localdomain>
2020-02-28 01:27:29 +0100

    update

--- P1/main.cpp
+++ P1/main.cpp
@@ -5,6 +5,7 @@
 #include <math.h>
 #include <random>
 #include "timer.cpp"
+#include "sort_scal.cpp"
 
 using namespace std;
 mt19937 generator{random_device{}()};
@@ -16,62 +17,6 @@ T random(T min, T max) {
 
 #define ASCENDING 0
 #define DESCENDING 1
-
-template <typename T>
-void scal(T A[], int len, int left, int mid, int right) {
-
-    int i = left;
-    int j = mid + 1;
-    int k = left;
-    T B[len];
-
-    while (i <= mid && j<= right) //przejdź przez wszystkie elementy
-    {
-        if (A[i] < A[j]) //porównywanie
-        {
-            B[k] = A[i];
-            i++;
-        }
-        else
-        {
-            B[k] = A[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i <= mid) //dopełnienie elementami z pierwszej połówki
-    {
-        B[k] = A[i];
-        i++;
-        k++;
-    }
-
-    while (j <= right) //dopełnienie elementami z drugiej połówki
-    {
-        B[k] = A[j];
-        j++;
-        k++;
-    }
-
-    for(i = left; i <= right; i++) //przepisanie elementów z B do A
-    {
-        A[i] = B[i]; 
-    }
-    
-}
-
-template <typename T>
-void sort_scal(T A[], int len, int left, int right) {
-    if (left < right)
-    {
-        int mid = floor((left+right)/2);    //liczenie środka
-        sort_scal<T>(A, len, left, mid);       //sortujemy lewą część
-        sort_scal<T>(A, len, mid+1, right);    //sortujemy prawą część
-        scal<T>(A, len, left, mid, right);     //scalamy obie części
-    }
-    
-}
 
 
 int* pick_array(int* A, int len, int min=0, int max=1000) {
---
+++ P1/sort_scal.cpp
@@ -0,0 +1,56 @@
+template <typename T>
+void scal(T A[], int len, int left, int mid, int right) {
+
+    int i = left;
+    int j = mid + 1;
+    int k = left;
+    T B[len];
+
+    while (i <= mid && j<= right) //przejdź przez wszystkie elementy
+    {
+        if (A[i] < A[j]) //porównywanie
+        {
+            B[k] = A[i];
+            i++;
+        }
+        else
+        {
+            B[k] = A[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i <= mid) //dopełnienie elementami z pierwszej połówki
+    {
+        B[k] = A[i];
+        i++;
+        k++;
+    }
+
+    while (j <= right) //dopełnienie elementami z drugiej połówki
+    {
+        B[k] = A[j];
+        j++;
+        k++;
+    }
+
+    for(i = left; i <= right; i++) //przepisanie elementów z B do A
+    {
+        A[i] = B[i]; 
+    }
+    
+}
+
+template <typename T>
+void sort_scal(T A[], int len, int left, int right) {
+    if (left < right)
+    {
+        int mid = floor((left+right)/2);    //liczenie środka
+        sort_scal<T>(A, len, left, mid);       //sortujemy lewą część
+        sort_scal<T>(A, len, mid+1, right);    //sortujemy prawą część
+        scal<T>(A, len, left, mid, right);     //scalamy obie części
+    }
+    
+}
+
